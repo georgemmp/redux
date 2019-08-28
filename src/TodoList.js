cf4ae6aea8455d56376739e73f83d7ad45c46181
georgemmp <machadogeorge.p@gmail.com>
2019-08-28 16:36:01 -0300

    proptypes

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const TodoList = (props) => (
     <ul>
         { props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
     </ul>
 );
+
+TodoList.prototype = {
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        text: PropTypes.string
+    })).isRequired
+}
 
 const mapStateToProps = state => ({
     todos: state.todos
