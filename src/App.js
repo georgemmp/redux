85a51f901038cea85bb7b1adbcd3daabed78463a
georgemmp <machadogeorge.p@gmail.com>
2019-08-29 21:37:48 -0300

    configuração do reactotron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import './config/ReactotronConfig';
+
 import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
-
 import TodoList from './TodoList';
 
 function App() {
