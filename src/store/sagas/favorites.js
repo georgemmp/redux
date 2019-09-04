a740626a706fe696574fabcc346925535a3fc7e9
georgemmp <machadogeorge.p@gmail.com>
2019-09-04 01:39:19 -0300

    Tratamento de adição de dados duplicados

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import api from '../../services/api';
 
@@ -8,14 +8,22 @@ export function* addFavorite(action) {
     try {
         const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-        const repositoryData = {
-            id: data.id,
-            name: data.full_name,
-            description: data.description,
-            url: data.html_url
-        };
+        const isDuplicated = yield select(state => state.favorites.data.find(
+            favorite => favorite.id === data.id
+        ));
 
-        yield put(Creators.addFavoriteSuccess(repositoryData));
+        if (isDuplicated) {
+            yield put(Creators.addFavoriteFailure('Repositório Duplicado'));
+        } else {
+            const repositoryData = {
+                id: data.id,
+                name: data.full_name,
+                description: data.description,
+                url: data.html_url
+            };
+    
+            yield put(Creators.addFavoriteSuccess(repositoryData));
+        }
     } catch (error) {
         yield put(Creators.addFavoriteFailure('Erro ao adicionar repositório'));
     }
